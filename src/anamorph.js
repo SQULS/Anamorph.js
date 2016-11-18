3ab9a5f7f80fd9ba03e11039d09a6f65447e426e
Rawbit <raw-bit@users.noreply.github.com>
2016-11-18 23:44:01 +0000

    Incorporated YouTube HTML5 API

--- src/anamorph.js
+++ src/anamorph.js
@@ -2,16 +2,18 @@ var an_media, an_overlay;
 var w = $(window).width() * 1.5;
 var h = $(window).height() * 1.5;
 function anamorph(an_media, an_overlay) {
+    $.getScript('https://www.youtube.com/iframe_api');
     var an_media_url = an_media.split('.');
     var an_media_id = an_media.split('/');
-    var an_length = an_media.split('.').length-1;
+    var an_length = an_media_url.length-1;
+    var an_yt_length = an_media_id.length-1;
     $(document.body).css({"margin": "0", "background": "none"});
     var an_style = "position: fixed; top: 50%; left: 50%; min-width: 100%; min-height: 100%; transform: translate(-50%, -50%); width=auto; height=auto";
     var an_wrapper = "<div id='an_wrapper' style='z-index: -997;'></div>";
     var an_over = "<div id='an_overlay' style='z-index: -998; position: fixed; top: 0; background-color: #000000; width: 100%; height: 100%;'></div>";
     var an_video = "<video id='an_video' style='z-index: -999; "+an_style+"' autoplay loop muted><source src="+an_media+" type='video/mp4'></video>";
     var an_img = "<div id='an_img' style='z-index: -999; "+an_style+"'><img style='"+an_style+"' src='"+an_media+"'></div>";
-    var an_youtube = "<iframe id='an_youtube' frameborder='0' width='"+w+"' height='"+h+"' src='"+an_media+"?autohide=1&;autoplay=1&;controls=0&;loop=1&;wmode=transparent&;playlist="+an_media_id[4]+"&;modestbranding=1&;iv_load_policy=3&;disablekb=1&;playsinline=1&;rel=0&;showinfo=0;' style='z-index:-999; "+an_style+"'></iframe>";
+    var an_youtube = "<div id='an_ytplayer' width='"+w+"' height='"+h+"' style='z-index: -999; "+an_style+"'></div>";
     $(an_wrapper).prependTo('body');
     if (an_overlay == 'bw') {
     $(an_over).appendTo('#an_wrapper').css('mix-blend-mode','saturation');
@@ -20,6 +22,41 @@ function anamorph(an_media, an_overlay) {
     }
     if (an_media.indexOf('youtube') !== -1) {
      $(an_youtube).appendTo('#an_wrapper');
+        window.onYouTubePlayerAPIReady = function() {
+            var player = new YT.Player('an_ytplayer', {
+                height: h,
+                width: w,
+                videoId: an_media_id[an_yt_length],
+                playerVars: {
+                    autoplay: 1,
+                    controls: 0,
+                    showinfo: 0,
+                    modestbranding: 1,
+                    loop: 1,
+                    fs: 0,
+                    cc_load_policty: 0,
+                    iv_load_policy: 3,
+                    autohide: 0,
+                    rel: 0,
+                    disablekb: 1,
+                    enablejsapi: 1,
+                    fs: 0,
+                    playsinline: 0
+                },
+                events: {
+                    onReady: function(e) {
+                            e.target.mute()
+                            e.target.loop()
+                    },
+                    onStateChange: 
+                            function(e){
+                                if (e.data === YT.PlayerState.ENDED) {
+                                player.playVideo(); 
+                            }
+    }
+                }
+        })
+        }
     } else {
     if (an_media_url[an_length] == 'jpg' | 'gif' | 'png') {
         $(an_img).appendTo('#an_wrapper');
