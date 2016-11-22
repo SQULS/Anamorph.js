3252806f9e57afd1fc8b9f27c223d2f802068ddc
Rawbit <raw-bit@users.noreply.github.com>
2016-11-22 12:14:51 +0000

    Fade in added

--- src/anamorph.js
+++ src/anamorph.js
@@ -7,21 +7,21 @@ function anamorph(an_media, an_overlay) {
     var an_media_id = an_media.split('/');
     var an_length = an_media_url.length-1;
     var an_yt_length = an_media_id.length-1;
-    $(document.body).css({"margin": "0", "background": "none"});
+    $(document.body).css({"margin": "0", "background": "#000000"});
     var an_style = "position: fixed; top: 50%; left: 50%; min-width: 100%; min-height: 100%; transform: translate(-50%, -50%); width=auto; height=auto";
-    var an_wrapper = "<div id='an_wrapper' style='z-index: -997;'></div>";
+    var an_wrapper = "<div id='an_wrapper' style='z-index: -997; opacity: 0;'></div>";
     var an_over = "<div id='an_overlay' style='z-index: -998; position: fixed; top: 0; background-color: #000000; width: 100%; height: 100%;'></div>";
     var an_video = "<video id='an_video' style='z-index: -999; "+an_style+"' autoplay loop muted><source src="+an_media+" type='video/mp4'></video>";
     var an_img = "<div id='an_img' style='z-index: -999; "+an_style+"'><img style='"+an_style+"' src='"+an_media+"'></div>";
     var an_youtube = "<div id='an_ytplayer' width='"+w+"' height='"+h+"' style='z-index: -999; "+an_style+"'></div>";
     $(an_wrapper).prependTo('body');
     if (an_overlay == 'bw') {
-    $(an_over).appendTo('#an_wrapper').css('mix-blend-mode','saturation');
+        $(an_over).appendTo('#an_wrapper').css('mix-blend-mode','saturation');
     } else if (an_overlay > 0) {
-    $(an_over).appendTo('#an_wrapper').css('opacity',an_overlay);
+        $(an_over).appendTo('#an_wrapper').css('opacity',an_overlay);
     }
-    if (an_media.indexOf('youtube') !== -1) {
-     $(an_youtube).appendTo('#an_wrapper');
+    if (an_media.indexOf('youtube.com' || 'youtu.be') !== -1) {
+        $(an_youtube).appendTo('#an_wrapper');
         window.onYouTubePlayerAPIReady = function() {
             var player = new YT.Player('an_ytplayer', {
                 height: h,
@@ -45,23 +45,32 @@ function anamorph(an_media, an_overlay) {
                 },
                 events: {
                     onReady: function(e) {
-                            e.target.mute()
-                            e.target.loop()
+                        e.target.mute()
                     },
                     onStateChange: 
-                            function(e){
-                                if (e.data === YT.PlayerState.ENDED) {
-                                player.playVideo(); 
-                            }
-    }
+                    function(e){
+                        if (e.data === YT.PlayerState.ENDED) {
+                            player.playVideo();
+                        }
+                        if (e.data === YT.PlayerState.PLAYING) {
+                            $('#an_wrapper').fadeTo("slow",1);
+                        }
+                    }
                 }
-        })
-        }
+            })
+            }
     } else {
-    if (an_media_url[an_length] == 'jpg' | 'gif' | 'png') {
-        $(an_img).appendTo('#an_wrapper');
-    } else if (an_media_url[an_length] == 'mp4') {
-        $(an_video).appendTo('#an_wrapper');
-    }
+        if (an_media_url[an_length] == 'jpg' | 'gif' | 'png') {
+            $(an_img).appendTo('#an_wrapper');
+            $('#an_img').ready(function() {
+                $('#an_wrapper').fadeTo("slow",1);
+            });
+        } else if (an_media_url[an_length] == 'mp4') {
+            $(an_video).appendTo('#an_wrapper');
+            var an_loadedvideo = document.getElementById('an_video');
+            an_loadedvideo.addEventListener('playing', function() {
+                $('#an_wrapper').fadeTo("slow",1);
+            }, false);
+        }
     }
 }
